387591725d4d18135d23976189275c71b7b7d6a4
philgebauer <gebauer.phil@gmail.com>
2020-05-08 16:59:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      fruits: ['apple', 'orange', 'banana'],
+      value: ''
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    onSubmit(event){
+      event.preventDefault();
+      this.setState({
+        fruits: [...this.state.fruits, this.state.value]
+      })
+    }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+        <h1>Hello</h1>
+        <p>Enter your name:</p>
+        <input
+          type="text" 
+          onChange={this.handleChange}
+        />
+        <button> Click me</button> 
+      </form>
+        <ul> 
+          {this.state.fruits.map((fruit) => (
+            <li key={fruit}> {fruit} </li> 
+          ))}
+        </ul> 
+      </div> 
   );
+  }
 }
 
 export default App;
